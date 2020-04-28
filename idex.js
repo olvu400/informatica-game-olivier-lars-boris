791816b06fece6f353d004c7aff2ed479b35a727
Olivier Drees <olivier.s.drees@gmail.com>
2020-04-28 11:49:05 +0000

    snake start toegevoegd

--- idex.js
+++ idex.js
@@ -1,1 +1,21 @@
+window.onload = funcion() {
+    var cvs = document.getElementById("canvas");
+    var ctx = cvs.getContext("2d");
 
+    var cvsW = cvs.width;
+    var cvsH = cvs.heigth;
+
+    var snakeW = 10;
+    var snakeH = 10;
+
+    function tekenSlang(x, y) {
+        ctx.fillStyle = "#FFF";
+        ctx.fillRect(x*snakeW, y*snakeH, snakeW, snakeH);
+
+        ctx.fillStyle = "#000";
+        ctx.strokeRect(x*snakeW, y*snakeH, snakeW, snakeH);
+    }
+
+
+
+}
