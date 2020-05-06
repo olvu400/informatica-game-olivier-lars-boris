2139ae784feebcedea2ee2620aba8738ee3b1660
Olivier Drees <olivier.s.drees@gmail.com>
2020-05-06 14:00:13 +0000

    fruit toegevoegd + snake body toegevoegd

--- draw.js
+++ draw.js
@@ -3,17 +3,23 @@ const ctx = canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
-
 var snake;
 
 (function setup() {
     snake = new Snake();
-    snake.draw();
+    fruit = new Fruit();
+    fruit.pickLocation();
 
     window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
         snake.update();
         snake.draw();
+
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+
     }, 250)
 }())
 
